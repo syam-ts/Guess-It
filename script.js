3b997c73b4543b20ddb3419f8237f5e09c681a68
SYAM T S <141516071+Syam543@users.noreply.github.com>
2023-11-26 19:00:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function startGame() {
   alert("Let's Start");
   // The SecetNumber
-  var secretNumber = "tripura";
+  var secretNumber = "mannar";
   var userInput;
-  var count=0
-  userInput = prompt("Enter the Indian State Name");
+  var count = 1
+  userInput = prompt("Enter the Alppuzha Town Name");
   while (userInput != secretNumber) {
-    count++
-    userInput = prompt("YOU ARE FOOL : " + count);
+    if(count < 5){
+      count++
+      userInput = prompt("Looser : " + count);
+    }
   }
 alert(`${count < 5 ? 'YOU ARE SMART' : 'YOU ARE STUPID'}`);
   window.location.href = "success.html";
 }
-
