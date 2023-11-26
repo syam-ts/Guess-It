dc297d0cb4ed48ed9e3fb5267618d4649edeab7f
SYAM T S <141516071+Syam543@users.noreply.github.com>
2023-11-26 19:48:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function startGame() {
   alert("Let's Start");
-  // The SecetNumber
-  var secretNumber = "mannar";
+  // The SecetTest
+  var secretTest;
   var userInput;
+  var secretTest = prompt("What you want to test ?");
   var count = 1
-  userInput = prompt("Enter the Alppuzha Town Name");
-  while (userInput != secretNumber) {
+  userInput = prompt("Enter the Desired");
+  while (  secretTest != userInput) {
     if(count < 5){
       count++
-      userInput = prompt("Looser : " + count);
+      userInput = prompt("YOU ARE FOOL : " + count);
     }
   }
 alert(`${count < 5 ? 'YOU ARE SMART' : 'YOU ARE STUPID'}`);
