c40962853c6b6186d9a386c0034319868a47cb98
SYAM T S <141516071+Syam543@users.noreply.github.com>
2023-11-29 11:44:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function startGame() {
   var secretTest;
   var userInput;
   var secretTest = prompt("What you want to test ?");
+  var limit = prompt("Enter the limit");
   userInput = prompt("Enter the Desired");
    var count = 0
   while (  secretTest != userInput) {
-    if(count < 10){
+    if(count < limit){
       count++
       userInput = prompt("YOU ARE FOOL : " + count);
     }
   }
-alert(`${count <= 10 ? 'YOU ARE SMART' : 'YOU ARE STUPID'}`);
+alert(`${count <= limit ? 'YOU ARE SMART' : 'YOU ARE STUPID'}`);
   window.location.href = "success.html";
 }
 //Block game
